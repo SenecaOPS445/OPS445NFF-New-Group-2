3e17965cec28e3b0d6770a159c433c8ce9ebc1bb
Jude Owusu <jowusu9@myseneca.ca>
2025-04-07 02:03:35 -0400

    85% Complete

---
+++ get_os.py
@@ -0,0 +1,33 @@
+import argparse
+import shutil
+import os
+import socket
+import platform
+import psutil
+
+"Creating a class called system Report, It contains the fuctions that will help the script run"
+
+class SystemReport:
+    "Initialize system report"
+    def __init__(self):
+        self.disk_usage = None
+        self.uptime = None
+        self.os = None
+        self.dirs = None
+        self.cpu_info = None
+        self.ip = None
+
+
+    def get_os(self):
+        "Retrieval of the os information"
+
+        os_name = platform.system()
+        os_version = platform.version()
+        os_release = platform.release()
+
+        return f"{os_name} {os_release} (Ver: {os_version})"
+    
+if __name__ == "__main__":
+    report = SystemReport()
+    print("OS Info Test Output:")
+    print(report.get_os())
